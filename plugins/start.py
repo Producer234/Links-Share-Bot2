04aafc6f1588876f25a5535790f507f8e0a61611
Producer234 <prexplanation2@gmail.com>
2025-11-07 21:14:31 +0600

    Add comment for force subscription check

--- plugins/start.py
+++ plugins/start.py
@@ -43,7 +43,7 @@ async def start_command(client: Bot, message: Message):
             
     await add_user(user_id)
 
-     Check Force Subscription
+    # ✅ Check Force Subscription
     if not await is_subscribed(client, user_id):
         await temp.delete()
         return await not_joined(client, message)
